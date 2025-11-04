6b11b82602bcd948c9fc377d1ad60cc18026294e
f-raffa <raffaele.franco1981@gmail.com>
2025-11-04 08:28:46 +0100

    console serverless

---
+++ IXM eDW - POC/SQL Scripts/Console.sql
@@ -0,0 +1,10 @@
+
+-- contracts landing: 16147576
+select count(*) contracts_count
+  from aquarius_refined_landing.contracts;
+
+
+
+-- contracts staging: 567257
+select count(*) contracts_count
+  from aquarius_refined_staging.contracts;
